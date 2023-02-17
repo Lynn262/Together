ece26b6c6115bd5a1c2f034b6b4bef96032906ac
qixiaojian310 <1821059387@qq.com>
2023-02-18 02:20:04 +0800

    change the position to the fixed and add the blank card

--- src/utils/dragUtils.ts
+++ src/utils/dragUtils.ts
@@ -2,40 +2,46 @@ import { useEventListener } from "@vueuse/core";
 import { ref, Ref } from "vue";
 import { throttle } from "./throttle";
 
-type cardPositionType = "absolute" | "relative";
+type cardPositionType = "fixed" | "static";
 
-interface MousePositionTransform {
+//鼠标相对于整个窗口的偏移
+export interface MousePositionTransform {
 	x: number;
 	y: number;
 	press: boolean;
 }
+//卡片本身的偏移
+export interface CardPositionTransform {
+	x: number;
+	y: number;
+}
 
-interface CardPositionTransform {
+interface CardInitPosition {
 	x: number;
 	y: number;
 }
 
 export interface DragCardStyleInterface {
-	position: cardPositionType;
+	margin: number;
 	left: String;
 	top: String;
+	height: String;
+	width: String;
 }
 
-//鼠标相对于整个窗口的偏移
-const mousePositionTransform: Ref<MousePositionTransform> = ref({
+const cardInitPosition: CardInitPosition = {
 	x: 0,
 	y: 0,
-	press: false,
-});
-//卡片本身的偏移
-const cardPositionTransform: Ref<CardPositionTransform> = ref({
-	x: 0,
-	y: 0,
-});
+};
 
-export const initDragUtil = (e: MouseEvent, dragCardStyleRef: Ref<DragCardStyleInterface>) => {
+export const initDragUtil = (e: MouseEvent, dragCardStyleRef: Ref<DragCardStyleInterface>, mousePositionTransform: Ref<MousePositionTransform>, cardPositionTransform: Ref<CardPositionTransform>, isDrag: Ref<boolean>) => {
 	e.preventDefault();
-	dragCardStyleRef.value.position = "absolute";
+	dragCardStyleRef.value.left = `${(e.currentTarget as Element).getBoundingClientRect().left - dragCardStyleRef.value.margin}px`;
+	dragCardStyleRef.value.top = `${(e.currentTarget as Element).getBoundingClientRect().top - dragCardStyleRef.value.margin}px`;
+	dragCardStyleRef.value.height = `${(e.currentTarget as Element).getBoundingClientRect().height}px`;
+	dragCardStyleRef.value.width = `${(e.currentTarget as Element).getBoundingClientRect().width}px`;
+	cardInitPosition.x = (e.currentTarget as Element).getBoundingClientRect().left - dragCardStyleRef.value.margin;
+	cardInitPosition.y = (e.currentTarget as Element).getBoundingClientRect().top - dragCardStyleRef.value.margin;
 	mousePositionTransform.value.x = e.clientX;
 	mousePositionTransform.value.y = e.clientY;
 	//鼠标已经被按下
@@ -46,12 +52,12 @@ export const initDragUtil = (e: MouseEvent, dragCardStyleRef: Ref<DragCardStyleI
 				window,
 				"mousemove",
 				(ev) => {
-					dragMoveUtil(ev, dragCardStyleRef);
+					dragMoveUtil(ev, dragCardStyleRef, mousePositionTransform, cardPositionTransform);
 				},
 				true
 			);
 		},
-		50,
+		30,
 		false
 	);
 
@@ -59,30 +65,33 @@ export const initDragUtil = (e: MouseEvent, dragCardStyleRef: Ref<DragCardStyleI
 		window,
 		"mouseup",
 		(ev) => {
-			destroyDragUtil(ev, dragCardStyleRef);
+			destroyDragUtil(ev, dragCardStyleRef, isDrag, mousePositionTransform, cardPositionTransform);
 		},
 		true
 	);
-	//要实现拖动，首先要求子节点变为position：absolute
+	//要实现拖动，首先要求子节点变为position：fixed
 };
 
-const destroyDragUtil = (e: MouseEvent, dragCardStyleRef: Ref<DragCardStyleInterface>) => {
+const destroyDragUtil = (e: MouseEvent, dragCardStyleRef: Ref<DragCardStyleInterface>, isDrag: Ref<boolean>, mousePositionTransform: Ref<MousePositionTransform>, cardPositionTransform: Ref<CardPositionTransform>) => {
 	mousePositionTransform.value = {
 		x: 0,
 		y: 0,
 		press: false,
 	};
-	dragCardStyleRef.value.position = "relative";
+	cardPositionTransform.value = {
+		x: 0,
+		y: 0,
+	};
 	dragCardStyleRef.value.left = "0px";
 	dragCardStyleRef.value.top = "0px";
+	isDrag.value = false;
 };
 
-const dragMoveUtil = (e: MouseEvent, dragCardStyleRef: Ref<DragCardStyleInterface>) => {
+const dragMoveUtil = (e: MouseEvent, dragCardStyleRef: Ref<DragCardStyleInterface>, mousePositionTransform: Ref<MousePositionTransform>, cardPositionTransform: Ref<CardPositionTransform>) => {
 	if (mousePositionTransform.value.press) {
 		cardPositionTransform.value.x = e.clientX - mousePositionTransform.value.x;
 		cardPositionTransform.value.y = e.clientY - mousePositionTransform.value.y;
-		dragCardStyleRef.value.left = `${cardPositionTransform.value.x}px`;
-		dragCardStyleRef.value.top = `${cardPositionTransform.value.y}px`;
-		console.log(cardPositionTransform.value.y);
+		dragCardStyleRef.value.left = `${cardInitPosition.x + cardPositionTransform.value.x}px`;
+		dragCardStyleRef.value.top = `${cardInitPosition.y + cardPositionTransform.value.y}px`;
 	}
 };
